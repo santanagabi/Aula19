93c5b62876d7b9837d2ce39c7247b5d84a4be24a
Gabrielle <gaboliveirasantana1@gmail.com>
2022-06-30 16:45:13 -0300

    Adicionando projeto aula 19

---
+++ Aula19/Aula19/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+using Aula19.ViewModels;
+
+namespace Aula19
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            base.Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Define qual será o Controlador padrão
+            // Nota: O controlador MainViewModel ainda não foi criado nessa etapa.
+            base.DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ Aula19/Aula19/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+
+namespace Aula19.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+        private string nomeCompleto;
+
+        public string NomeCompleto
+        {
+            get
+            {
+                return nomeCompleto;
+            }
+            set
+            {
+                nomeCompleto = value;
+                // Observa se o atributo/instância FirstName recebeu alguma atualização.
+                base.NotifyOfPropertyChange(() => NomeCompleto);
+            }
+        }
+    }
+}
